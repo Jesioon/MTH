18077ec8381ae99b747db33823715433086b10b7
Jesioon <piotrlbl40@gmail.com>
2021-10-10 20:40:45 +0200

    RWD v4

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,19 +3,52 @@ import '../styles/Navigation.css';
 import {Link, NavLink} from 'react-router-dom';
 import logo from '../images/logo.png';
 import { FaBars } from "react-icons/fa";
-import { FaChevronUp } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa";
 
 class Navigation extends React.Component {
 
     state = {
-        showLeftMenu: false
+        showLeftMenu: false,
+        windowWidth: "",
+        windowHeight: ""
     }
 
     handleMenuClick() {
-        this.setState({
-            showLeftMenu: !this.state.showLeftMenu
-        })
+        const windowHeight = window.scrollY;
 
+        this.setState({
+            showLeftMenu: !this.state.showLeftMenu,
+            windowHeight
+        })
+        // Odwrócone bo state jeszcze nie jest ustalony
+        document.body.style.overflow = !this.state.showLeftMenu ? 'hidden' : 'unset';
+    }
+
+    handleResize = e => {
+        const windowWidth = window.innerWidth;
+        if(windowWidth > 980 && this.state.showLeftMenu) {
+        this.setState(prevState => {
+          return {
+            windowWidth,
+            showLeftMenu: false
+          };
+        });
+        document.body.style.overflow = 'unset';
+
+        }
+        this.setState(prevState => {
+          return {
+            windowWidth,
+          };
+        });
+      };
+    
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.handleResize);
     }
 
     render() {
@@ -30,21 +63,18 @@ class Navigation extends React.Component {
                 <li><NavLink to='/aboutUs'>o nas</NavLink></li>
             </ul>
             {!this.state.showLeftMenu && <span id='burger'><FaBars id='burgerIcon' onClick={this.handleMenuClick.bind(this)}/></span>}
-            {this.state.showLeftMenu && <span id='burger'><FaChevronUp id='burgerIcon' onClick={this.handleMenuClick.bind(this)}/></span>}
+            {this.state.showLeftMenu && <span id='burger'><FaChevronDown id='burgerIcon' onClick={this.handleMenuClick.bind(this)}/></span>}
 
             </nav>
-            {this.state.showLeftMenu && 
-                        <nav className='leftNav'>
-                        <ul>
-                            <li onClick={this.handleMenuClick.bind(this)}><NavLink to='/' exact>w sprzedaży</NavLink></li>
-                            <li onClick={this.handleMenuClick.bind(this)}><NavLink to='/inwestments'>inwestycje zrealizowane</NavLink></li>
-                            <li onClick={this.handleMenuClick.bind(this)}><NavLink to='/buyGrounds'>kupimy grunty</NavLink></li>
-                            <li onClick={this.handleMenuClick.bind(this)}><NavLink to='/aboutUs'>o nas</NavLink></li>
-                        </ul>
-                    </nav>
-            }
-
-
+            
+            <nav className="leftNav" style={{height: this.state.showLeftMenu ? `calc(100vh - 85px + ${this.state.windowHeight}px)` : '0'}}>
+                <ul>
+                    <li onClick={this.handleMenuClick.bind(this)}><NavLink to='/' exact>w sprzedaży</NavLink></li>
+                    <li onClick={this.handleMenuClick.bind(this)}><NavLink to='/inwestments'>inwestycje zrealizowane</NavLink></li>
+                    <li onClick={this.handleMenuClick.bind(this)}><NavLink to='/buyGrounds'>kupimy grunty</NavLink></li>
+                    <li onClick={this.handleMenuClick.bind(this)}><NavLink to='/aboutUs'>o nas</NavLink></li>
+                </ul>
+            </nav>
             </>
         );
     }
